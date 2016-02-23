1d0b672b8059862970e3eadb1ae271a75500b6e5
Liran Ashwal <liran.ashwal@gmail.com>
2016-02-23 13:09:39 +0200

    add components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
+import {LogOperations} from './log_operations/log.operations';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><log-operation></log-operation>',
+    directives:[LogOperations]
 })
 export class AppComponent implements OnInit{
     ngOnInit():any {
---
+++ app/log_operations/log.operations.js
@@ -0,0 +1,56 @@
+System.register(['angular2/core'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+        switch (arguments.length) {
+            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+        }
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var LogOperations;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            LogOperations = (function () {
+                function LogOperations() {
+                    this.arrData = [];
+                    this.addGarbageData();
+                }
+                LogOperations.prototype.getSignleGarbageData = function (data) {
+                    var tempData = {};
+                    tempData.operationName = data.operationName;
+                    tempData.date = data.date;
+                    return tempData;
+                };
+                LogOperations.prototype.addGarbageData = function () {
+                    var data1 = this.getSignleGarbageData({
+                        operationName: "Liran",
+                        date: 1316546
+                    });
+                    var data2 = this.getSignleGarbageData({
+                        operationName: "Ziv",
+                        date: 498215
+                    });
+                    this.arrData.push(data1, data2);
+                };
+                LogOperations = __decorate([
+                    core_1.Component({
+                        selector: 'log-operation',
+                        templateUrl: 'app/log_operations/log.operations.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], LogOperations);
+                return LogOperations;
+            })();
+            exports_1("LogOperations", LogOperations);
+        }
+    }
+});
+//# sourceMappingURL=log.operations.js.map
