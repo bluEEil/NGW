f9964a1e101c8be0eb7ecf3f7c557b80cbf04bd8
Ziv Glazer <zivking10@gmail.com>
2016-02-23 10:31:28 +0200

    first commit, basic angular 2 quick guide point with jquery ts dec

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent implements OnInit{
+    ngOnInit():any {
+
+    }
+}
