8004394030ecfa2ec3ec5ec994ad509984d94da5
Ziv Glazer <zivking10@gmail.com>
2016-02-24 10:16:51 +0200

    ziv added the table in the drawer

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,11 @@
 import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
 import {DrawerPanelComponent} from './drawer-panel.component';
-import {LogOperations} from './log_operations/log.operations';
 
 @Component({
     selector: 'my-app',
-    template: '<log-operation></log-operation>',
-    directives:[LogOperations, DrawerPanelComponent]
+    template: '<ngw-drawer-panel></ngw-drawer-panel>',
+    directives:[DrawerPanelComponent]
 })
 export class AppComponent implements OnInit{
     ngOnInit():any {
--- app/drawer-panel.component.ts
+++ app/drawer-panel.component.ts
@@ -3,6 +3,7 @@
  */
 import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
+import {LogOperations} from './log_operations/log.operations';
 
 @Component({
     selector: 'ngw-drawer-panel',
@@ -10,15 +11,21 @@ import {OnInit} from 'angular2/core';
 
     <paper-drawer-panel id="menuDrawerPanel" responsive-width="100000px">
 
-    <div drawer><h1>drawer section</h1></div>
+        <div drawer>
+            <h1>drawer section</h1>
+        </div>
+
         <div main>
             <paper-toolbar class="main">
                 <paper-icon-button icon="menu" (click)="toggleDrawer()"></paper-icon-button>
             </paper-toolbar>
+
+            <log-operation></log-operation>
         </div>
 
     </paper-drawer-panel>
-    `
+    `,
+    directives: [LogOperations]
 })
 export class DrawerPanelComponent{
 
