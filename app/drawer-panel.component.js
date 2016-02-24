7cfc15b953443f4d47fbaeef636ac350613468c7
Ziv Glazer <zivking10@gmail.com>
2016-02-24 10:26:43 +0200

    blah

--- app/drawer-panel.component.js
+++ app/drawer-panel.component.js
@@ -30,7 +30,7 @@ System.register(['angular2/core', './log_operations/log.operations'], function(e
                 DrawerPanelComponent = __decorate([
                     core_1.Component({
                         selector: 'ngw-drawer-panel',
-                        template: "\n\n    <paper-drawer-panel id=\"menuDrawerPanel\" responsive-width=\"100000px\">\n\n        <div drawer>\n            <h1>drawer section</h1>\n        </div>\n\n        <div main>\n            <paper-toolbar class=\"main\">\n                <paper-icon-button icon=\"menu\" (click)=\"toggleDrawer()\"></paper-icon-button>\n            </paper-toolbar>\n\n            <!--<log-operation></log-operation>-->\n        </div>\n\n    </paper-drawer-panel>\n    ",
+                        template: "\n\n    <paper-drawer-panel id=\"menuDrawerPanel\" responsive-width=\"100000px\">\n\n        <div drawer>\n            <h1>drawer section</h1>\n        </div>\n\n        <div main>\n            <paper-toolbar class=\"main\">\n                <paper-icon-button icon=\"menu\" (click)=\"toggleDrawer()\"></paper-icon-button>\n            </paper-toolbar>\n\n            <log-operation></log-operation>\n        </div>\n\n    </paper-drawer-panel>\n    ",
                         directives: [log_operations_1.LogOperations]
                     }), 
                     __metadata('design:paramtypes', [])
